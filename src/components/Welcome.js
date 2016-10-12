84dd230c79293f8dcae5b4221c847f9cbae0e9ff
HONG ZHOU <holly_zhou@yahoo.com>
2016-10-11 21:31:21 -0700

    make nicely

---
+++ src/components/Welcome.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class Welcome extends Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="jumbotron">
+            <h1 className="text-center">Welcome to Twitter App</h1>
+            <p className='intro'>Welcome to our app, created by Holly and John. Search tweets on our 'Search' Page. Hit the save button to save a tweet. Check out your saved tweets on the 'Saved Tweets' page. </p>
+          </div>
+        </div>
+      </div>
+    )
+  }
+};
--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import Layout from './components/Layout';
 import SearchPage from './components/SearchPage';
 import SavedTweets from './components/SavedTweets';
+import Welcome from './components/Welcome';
 
 
 injectTapEventPlugin();
@@ -13,6 +14,7 @@ injectTapEventPlugin();
 render(
   <Router history={browserHistory}>
     <Route path='/' component={Layout}>
+     <IndexRoute component={Welcome}></IndexRoute>
       <Route path='searchPage' component={SearchPage}/>
       <Route path='savedTweets' component={SavedTweets}/>
     </Route>
